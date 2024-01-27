91b0a1d26a63424af17b2124db8c74b7ac3b92fa
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-01-27 15:23:52 +0800

    Add files via upload

--- source/OldTVShader.hx
+++ source/OldTVShader.hx
@@ -8,21 +8,20 @@ class OldTVShader extends FlxShader
 {
 	@:glFragmentSource('
         #pragma header
-        #define id vec2(0.,1.)
-        #define k 1103515245U
+        #define id vec2(0.0,1.0)
         #define PI 3.141592653
-        #define TAU PI * 2.
+        #define TAU (PI * 2.0)
 
         uniform float iTime;
 
         //prng func, from https://stackoverflow.com/a/52207531
-        vec3 hash(uvec3 x) {
-            x = ((x>>8U)^x.yzx)*k;
-            x = ((x>>8U)^x.yzx)*k;
-            x = ((x>>8U)^x.yzx)*k;         
-            return vec3(x)*(1.0/float(0xffffffffU));
-        }
 
+        vec3 hash(vec3 p) {
+			p = fract(p * 0.1031);
+			p += dot(p, p.yzx + 33.33);
+			return fract(vec3((p.x + p.y) * p.z, (p.y + p.z) * p.x, (p.z + p.x) * p.y));
+		}
+        
         void main() {
             bool flag = false;
             bool flag2 = false;
@@ -40,16 +39,16 @@ class OldTVShader extends FlxShader
             
             float realSize = lineSize / openfl_TextureSize.y / 2.0;
             float position = mod(iTime, timeMod) / time;
-            float position2 = 99.;
+            float position2 = 99.0;
             if (iTime > repeatTime) {
                 position2 = mod(iTime - repeatTime, timeMod) / time;
             }
             if (!(uv.y - position > realSize || uv.y - position < -realSize)) {
-                uv.x -= hash(uvec3(0., uv.y * uvyMul, iTime * updateRate2)).x * offsetMul;
+                uv.x -= hash(uvec3(0.,0 uv.y * uvyMul, iTime * updateRate2)).x * offsetMul;
                 flag = true;
-            } else if (position2 != 99.) {
+            } else if (position2 != 99.0) {
                 if (!(uv.y - position2 > realSize || uv.y - position2 < -realSize)) {
-                    uv.x -= hash(uvec3(0., uv.y * uvyMul, iTime * updateRate2)).x * offsetMul;
+                    uv.x -= hash(uvec3(0.0, uv.y * uvyMul, iTime * updateRate2)).x * offsetMul;
                     flag = true;
                 }
             }
@@ -70,7 +69,7 @@ class OldTVShader extends FlxShader
             col /= quality * directions - 14.0;
             
             //for the black on the left
-            if (uv.x < 0.) {
+            if (uv.x < 0.0) {
                 col = id.xxxy;
                 flag = false;
                 flag2 = true;
@@ -82,9 +81,9 @@ class OldTVShader extends FlxShader
             float cutoff2 = 0.92;
             float valMul2 = 0.007;
             
-            float val2 = hash(uvec3(uv.y * uvyMul3, 0., iTime * updateRate4)).x;
+            float val2 = hash(uvec3(uv.y * uvyMul3, 0.0, iTime * updateRate4)).x;
             if (val2 > cutoff2) {
-                float adjVal2 = (val2 - cutoff2) * valMul2 * (1. / (1. - cutoff2));
+                float adjVal2 = (val2 - cutoff2) * valMul2 * (1.0 / (1.0 - cutoff2));
                 if (uv.x < adjVal2) {
                     col = id.xxxy;
                     flag2 = true;
@@ -97,7 +96,7 @@ class OldTVShader extends FlxShader
             if (!flag2) {
                 float updateRate = 100.0;
                 float mixPercent = 0.05; 
-                col = mix(col, vec4(hash(uvec3(uv * openfl_TextureSize, iTime * updateRate)).rrr, 1.), mixPercent);
+                col = mix(col, vec4(hash(uvec3(uv * openfl_TextureSize, iTime * updateRate)).rrr, 1.0), mixPercent);
             }
             
             //white sploches
@@ -112,9 +111,9 @@ class OldTVShader extends FlxShader
                 float val = hash(uvec3(uv.x * uvxMul, uv.y * uvyMul2, iTime * updateRate3)).x;
                 if (val > cutoff) {
                     float offset = hash(uvec3(uv.y * uvyMul2, uv.x * uvxMul, iTime * updateRate3)).x;
-                    float adjVal = (val - cutoff) * valMul * (1. / (1. - cutoff));
+                    float adjVal = (val - cutoff) * valMul * (1.0 / (1.0 - cutoff));
                     adjVal -= abs((uv.x * uvxMul - (floor(uv.x * uvxMul) + offset)) * falloffMul);
-                    adjVal = clamp(adjVal, 0., 1.);
+                    adjVal = clamp(adjVal, 0.0, 1.0);
                     col = vec4(mix(col.rgb, id.yyy, adjVal), col.a);
                 }
             }
