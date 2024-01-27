f6faea69a7d5a740901331a22c29b707dc2f7de6
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-01-27 16:44:14 +0800

    Update CamDupeShader.hx

--- source/CamDupeShader.hx
+++ source/CamDupeShader.hx
@@ -37,7 +37,7 @@ uniform bool mirrorS;
 
 void main()
 {
-        vec2 uv = openfl_TextureCoordv*openfl_TextureSize/openfl_TextureSize.xy;
+        vec2 uv = openfl_TextureCoordv*openfl_TextureSize;
             uv.x *= multi;
             uv.y *= multi;
         uv = fract(uv);
