2d0a8894bb0ba2d1f6023ca3831b635e85f72f57
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-01-28 21:24:00 +0800

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -302,6 +302,11 @@ class CreditsState extends MusicBeatState
 			FlxG.sound.playMusic(Paths.music('creditsmenu'), 1);
 		}
 
+		#if android
+		addVirtualPad(UP_DOWN, B);
+		addPadCamera();
+		#end
+		
 		super.create();
 	}
 
@@ -319,9 +324,9 @@ class CreditsState extends MusicBeatState
 				MusicBeatState.switchState(new MainMenuState());
 			}
 
-			if (FlxG.mouse.wheel < 0)
+			if (controls.UI_UP_P)
 				newPos += 40;
-			if (FlxG.mouse.wheel > 0)
+			if (controls.UI_DOWN_P)
 				newPos -= 40;
 
 			if(newPos < 200)
