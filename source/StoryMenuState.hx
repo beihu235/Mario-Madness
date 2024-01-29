8838384e31db1c8ee3216e35fb25b86152e612ed
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-01-29 19:47:58 +0800

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -237,7 +237,7 @@ class StoryMenuState extends MusicBeatSubstate
 	override function update(elapsed:Float)
 	{
 		tottalTimer += elapsed;
-		if (controls.ACCEPT && quieto)
+		if (controls.ACCEPT)
 		{
 			selectWeek();
 		}
