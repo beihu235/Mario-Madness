42626ad1e2963ab42929aaaf800611200a95a70e
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-01-30 11:16:06 +0800

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -237,7 +237,7 @@ class StoryMenuState extends MusicBeatSubstate
 	override function update(elapsed:Float)
 	{
 		tottalTimer += elapsed;
-		if (controls.ACCEPT)
+		if (controls.ACCEPT #if android || _virtualpad.buttonA.justPressed #end && quieto)
 		{
 			selectWeek();
 		}
